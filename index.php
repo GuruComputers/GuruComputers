ad8c161e763f971a974226023db2f9499a7c9c41
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-11 16:00:38 +0000

    Add Website Hosting Icon

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 				<img src="img/icons/laptop_repairs.png" alt="Laptop Repairs" class="icon c6">
 				<br />
 				<img src="img/icons/printing_services.png" alt="Printing Services" class="icon c7">
-				<img src="img/icons/cd_printing.png" alt="CD Printing & Duplication" class="icon c8">
+				<img src="img/icons/website_hosting.png" alt="Website Hosting" class="icon c8">
 				<br />			
 			</div>
 		</section>
