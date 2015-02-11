082d123c9fe8bfb68240d81d101a96c23d9dc583
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-11 16:34:04 +0000

    divs added for pairs

--- index.php
+++ index.php
@@ -25,17 +25,25 @@
 		<section>
 			<div class="sec-one">
 				<img src="img/logo.png" alt="Guru Computers Logo" class="logo">
-				<img src="img/icons/cd_printing.png" alt="CD Printing & Duplication" class="icon c1">
-				<img src="img/icons/webdev.png" alt="Website Development" class="icon c2">
+				<div class="pair-one">
+					<img src="img/icons/cd_printing.png" alt="CD Printing & Duplication" class="icon c1">
+					<img src="img/icons/webdev.png" alt="Website Development" class="icon c2">
+				</div>
 				<br />
-				<img src="img/icons/computer_repairs.png" alt="Computer Repairs" class="icon c3">
-				<img src="img/icons/mobile_repairs.png" alt="Mobile Repairs" class="icon c4">
+				<div class="pair-two">
+					<img src="img/icons/computer_repairs.png" alt="Computer Repairs" class="icon c3">
+					<img src="img/icons/mobile_repairs.png" alt="Mobile Repairs" class="icon c4">
+				</div>
 				<br />
-				<img src="img/icons/console_repairs.png" alt="Console Repairs" class="icon c5">
-				<img src="img/icons/laptop_repairs.png" alt="Laptop Repairs" class="icon c6">
+				<div class="pair-three">
+					<img src="img/icons/console_repairs.png" alt="Console Repairs" class="icon c5">
+					<img src="img/icons/laptop_repairs.png" alt="Laptop Repairs" class="icon c6">
+				</div>
 				<br />
-				<img src="img/icons/printing_services.png" alt="Printing Services" class="icon c7">
-				<img src="img/icons/website_hosting.png" alt="Website Hosting" class="icon c8">
+				<div class="pair-four">
+					<img src="img/icons/printing_services.png" alt="Printing Services" class="icon c7">
+					<img src="img/icons/website_hosting.png" alt="Website Hosting" class="icon c8">
+				</div>
 				<br />			
 			</div>
 		</section>
