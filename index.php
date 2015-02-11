270dd0243345a30996f1d0eeebd42f45789c4a46
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-11 10:58:02 +0000

    index.php header started

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Need professional Services? Let us do I.T.">
+		<meta name="keywords" content="Guru Cpmputers Ltd"/>
+		<meta property="og:title" content='Guru Cpmputers Ltd'/>
+		<meta property="og:type" content="article"/>
+		<meta property="og:image" content="./img/logo.png"/>
+		<meta property="og:description" content="Need professional Services? Let us do I.T."/>
+		<meta name="twitter:card" content="summary"/>
+		<meta name="twitter:title" content='Guru Cpmputers Ltd'/>
+		<meta name="twitter:description" content="Need professional Services? Let us do I.T."/>
+		<meta name="twitter:image" content="./img/logo.png"/>
+		<title>Guru Computers Ltd</title>
+		<link media="all" type="text/css" rel="stylesheet" href="../css/normalize.css">
+		<link media="all" type="text/css" rel="stylesheet" href="../css/main.css">
+	</head>
+	
